09d84d000b09ce2d60ea9cb490a635cc9c00dda9
Jerry Tong <zihangtong@gmail.com>
2022-02-10 21:38:49 -0500

    commit tree 46772dcad14d7097471b73db26e2fdc8a93e8bb3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_res
 
+  # private
+
+  # def render_unprocessable_entity_res(invalid)
+  #   render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  # end
 end
